069332b71ed5aeee26b5db7c6483d50c0e08b63c
Stuart Tottle <stuart.tottle@ldn-m-0013.lan>
2019-05-05 21:20:04 +0100

    add elay

--- src/pageData/services/HttpService.ts
+++ src/pageData/services/HttpService.ts
@@ -1,6 +1,6 @@
 import { empty, Observable } from 'rxjs';
 import { ajax, AjaxError } from 'rxjs/ajax';
-import { catchError } from 'rxjs/operators';
+import { catchError, delay } from 'rxjs/operators';
 
 export class HttpService {
   private readonly baseUrl = 'https://cdn.telecineplay.com.br/api';
@@ -8,7 +8,7 @@ export class HttpService {
   get<T>(url: string): Observable<T> {
     const newUrl = `${this.baseUrl}${url}`;
     return ajax.getJSON<T>(newUrl).pipe(
-      // delay(2000),
+      delay(2000),
       catchError(error => this.handleError(error))
     );
   }

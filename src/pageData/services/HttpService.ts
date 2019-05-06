3d1bda9cdf31f7720c519a391061274d02acd9ae
Stuart Tottle <stuart.tottle@ldn-m-0013.lan>
2019-05-06 21:10:25 +0100

    stuff

--- src/pageData/services/HttpService.ts
+++ src/pageData/services/HttpService.ts
@@ -1,10 +1,10 @@
 import { empty, Observable } from 'rxjs';
 import { ajax, AjaxError } from 'rxjs/ajax';
 import { catchError, delay } from 'rxjs/operators';
+import { baseApiUrl } from '../env';
 
 export class HttpService {
-  private readonly baseUrl = 'https://cdn.telecineplay.com.br/api';
-  // private readonly baseUrl = 'https://demo-cdn.massiveaxis.com/api';
+  private readonly baseUrl = baseApiUrl;
 
   get<T>(url: string): Observable<T> {
     const newUrl = `${this.baseUrl}${url}`;

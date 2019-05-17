4ffb0c951e9235bd95e015c56711ae375f5b12c7
Stuart Tottle <stuart.tottle@ldn-m-0013.lan>
2019-05-17 21:45:31 +0100

    tidy up

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,6 +1,6 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { Subject } from 'rxjs';
-import { map, tap } from 'rxjs/operators';
+import { map, takeUntil, tap } from 'rxjs/operators';
 import { historyContext, RouterOutletProvider } from './RouterContext';
 import { BrowserHistory } from './services/BrowserHistory';
 import { BrowserRouter } from './services/BrowserRouter';
@@ -29,30 +29,31 @@ interface HistoryOutletState<T> {
 }
 
 const HistoryOutlet = <T extends {}>(props: HistoryOutletProps<T>) => {
-  const [element, setElement] = useState<HistoryOutletState<T>>();
+  const [route, setRoute] = useState<HistoryOutletState<T>>();
 
   useEffect(() => {
-    const destory$ = new Subject();
+    const destroy$ = new Subject();
     const { history, router } = props;
 
     history.activatedPath$
       .pipe(
+        takeUntil(destroy$),
         map(path => router.matchRoute(path)),
         map(route => ({
           element: route.template
         })),
-        tap(state => setElement(state))
+        tap(state => setRoute(state))
       )
       .subscribe();
 
     return () => {
-      destory$.next();
-      destory$.complete();
+      destroy$.next();
+      destroy$.complete();
     };
   }, []);
 
-  return element ? (
-    <RouterOutletProvider children={props.children} value={element.element} />
+  return route ? (
+    <RouterOutletProvider children={props.children} value={route.element} />
   ) : (
     <span />
   );
